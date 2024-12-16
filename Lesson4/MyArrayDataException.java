11f129afe01da191f3f31c3abd0585e8312cae63
Niklove168 <Nik210790@gmail.com>
2024-12-16 13:31:38 +0300

    Add files via upload

---
+++ Lesson4/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package Task4;
+
+public class MyArrayDataException extends Exception{
+    public MyArrayDataException(String message){
+        super(message);
+    }
+}
---
+++ Lesson4/MyArraySizeException.java
@@ -0,0 +1,9 @@
+package Task4;
+import java.util.Arrays;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+
+}
